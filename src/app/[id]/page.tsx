198ce700f9f391fa9e9a6ef4026ab9425bd9dbc7
Zhousiru <i@zhousiru.com>
2024-05-13 00:09:46 +0800

    feat: responsive layout for album detail page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -12,14 +12,17 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <>
       <BlurryBackground src={album.cover} />
-      <div className="mx-auto mt-[15vh] grid max-w-screen-2xl grid-cols-[auto_1fr] gap-24">
-        <div className="relative aspect-square w-[350px] overflow-hidden rounded-lg shadow-2xl">
+      <div className="mx-auto mt-[15vh] grid max-w-md gap-8 p-4 lg:max-w-screen-2xl lg:grid-cols-[auto_1fr] lg:gap-x-12 lg:p-12 xl:gap-x-24">
+        <div className="relative aspect-square w-full place-self-center overflow-hidden rounded-lg shadow-2xl lg:w-[350px]">
           <Image src={album.cover} alt="Album cover" fill />
         </div>
         <div className="flex flex-col justify-center gap-4">
           <div className="text-white">
-            <AutoSizingText className="font-medium" text={album.name} />
-            <h2 className="text-4xl">{album.publisher}</h2>
+            <AutoSizingText
+              className="font-medium leading-tight"
+              text={album.name}
+            />
+            <h2 className="text-2xl">{album.publisher}</h2>
           </div>
           <div className="flex gap-2 text-white opacity-75">
             {album.meta.map((v) => (
@@ -29,6 +32,10 @@ export default async function Page({ params }: { params: { id: string } }) {
           <button className="grid h-12 w-36 place-items-center rounded-full border bg-white transition hover:opacity-75">
             <IconPlayerPlay />
           </button>
+        </div>
+
+        <div className="w-full justify-self-center bg-white/25 lg:col-span-2 lg:max-w-screen-md xl:col-start-2 xl:justify-self-start">
+          Test
         </div>
       </div>
     </>
